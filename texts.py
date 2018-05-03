2be012dfe3879fe885d11614c31e579640a336eb
Kaeldas <38050534+Kaeldas@users.noreply.github.com>
2018-05-03 09:48:10 -0300

    Add rules button

--- texts.py
+++ texts.py
@@ -1,7 +1,11 @@
 import core
+from telegram import InlineKeyboardMarkup, InlineKeyboardButton
 ###bot is the Bot class in bot.py
 
 def welcome(bot, user):
     last = bot.chain.getLast()
-    core.sendMessage(bot.bot, "Welcome! Please add @" + last + " in your bio!")
+    core.sendMessage(bot.bot, "Welcome! Please add @" + last + " in your bio!", 
+                     rules_btn=InlineKeyboardMarkup(
+                         [[InlineKeyboardButton(text='Read Rules',
+                                               url='t.me/Bio_Chain_2_Rules')]]))
     
