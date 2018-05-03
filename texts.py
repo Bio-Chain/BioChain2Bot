e29b7538a6820a275f21433e3f7d7b1ff320eba6
Kaeldas <38050534+Kaeldas@users.noreply.github.com>
2018-05-03 16:42:55 -0300

    Delete useless variable

--- texts.py
+++ texts.py
@@ -3,7 +3,6 @@ from telegram import InlineKeyboardMarkup, InlineKeyboardButton
 ###bot is the Bot class in bot.py
 
 def welcome(bot, user):
-    last = bot.chain.getLast()
     core.sendMessage(bot.bot, "Welcome! Please add @" + last + " in your bio!", 
                      rules_btn=InlineKeyboardMarkup(
                          [[InlineKeyboardButton(text='Read Rules',
